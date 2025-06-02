667d2c91682740bb8d1ba8d1c6eb8e73dfa72c21
Pedro Braga de Lima <p1braga.tech@gmail.com>
2025-06-02 11:50:34 -0300

    Add files via upload

---
+++ Estudos02.java
@@ -0,0 +1,10 @@
+public class Estudos02 {
+    public static void main(String[] args){
+        int varint = 1;
+
+        System.out.print("valor: "+varint);
+        System.out.println("olá mundo!!!!!!!!");
+
+
+    }
+}
